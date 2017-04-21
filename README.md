1964a630a72a845d98ce5631c31050e9357d9701
Mate Szalay-Beko <mate.szalay-beko@flixbus.com>
2017-04-21 11:17:21 +0200

    adding the homework

---
+++ README.md
@@ -0,0 +1,29 @@
+# Spark Streaming Homework
+
+The tasks are about to process IMDB voting events and providing a "real-time" feedback about who is the best actor at the moment. In the current code, for some reason it always prints out "Chuck Norris". If you have other opinion, then it's better to fix it :)
+
+So the homework is to finish the tasks on the StreamingExample.scala file. *Task1 is mandatory*, Task2 and Task3 are optional. (Task2 is more complex, but if you finished it then Task3 will be very easy).
+
+
+# Input data
+The real data was downloaded from IMDB. We are processing only movies from the TOP-250 list, and also aggregating the votes a bit. Now in each microbatch a single year of votes are processed. I also provide some small test data files to enable debugging and showing an example for the expected results. You should use the ImdbStreamingExampleTest.scala to run the streaming applications on the datasets.
+
+In the ```actor_data``` files you have three columns separated by tabulators: actor name, movie title, movie year; while in the ```movie_rating``` files the columns are: rating (1-10), movie title, movie year. Each line in the movie rating file corresponds to 1000 similar votes in the real life. It is possible that there are different movies with the same title in different years, so you need to identify the movie by both title and year.
+
+To make the homework easier, I generated the input files in the way that you can expect **all the votes on a movie arriving in the year of its release**. So in other words: you don't need to care now with *late arrivals*.
+
+
+## Setting up the development environment
+
+The development environment is quite straight forward, you can run the whole application from IntelliJ.
+
+- You should clone this repo, and import the maven project into IntelliJ
+- You might need to setup the scala SDK (use scala 2.11.8), maybe also install Scala plugin if it is missing
+- Also you might need to mark the main/src/scala folder as "sources root" and the main/test/scala folder as "test sources root"
+- Then you should be able to run the individual unit tests
+
+Also you can use the preconfigured development environment from the Spark Core homework, described / downloadable from here: https://github.com/tewecske/sparktraininghomework
+
+
+
+
---
+++ src/test/scala/com/epam/streaming/ImdbStreamingExampleTest.scala
@@ -0,0 +1,93 @@
+package com.epam.streaming
+
+import org.junit.Test
+
+
+class ImdbStreamingExampleTest {
+
+  val INPUT_ACTORS_TOP_250_IMDB_MOVIES = "data/streaming_data/actor_data"
+  val INPUT_RATING_EVENTS_TOP_250_IMDB_MOVIES = "data/streaming_data/movie_ratings"
+
+  val INPUT_ACTORS_TEST = "data/streaming_data_test/actor_data"
+  val INPUT_RATING_EVENTS_TEST = "data/streaming_data_test/movie_ratings"
+
+  @Test
+  def shouldCalculateTopActors() = {
+    val minimumNumberOfVotes = 5000000
+    val minimumNumberOfMovies = 5
+
+    StreamingExample.main(Array(
+      INPUT_ACTORS_TOP_250_IMDB_MOVIES,
+      INPUT_RATING_EVENTS_TOP_250_IMDB_MOVIES,
+      minimumNumberOfVotes.toString,
+      minimumNumberOfMovies.toString
+    ))
+
+    // ===== send me the results, after the whole stream processed!   =====
+
+  }
+
+  @Test
+  def shouldCalculateTopActorsOnSmallTestDataSet() = {
+    val minimumNumberOfVotes = 0
+    val minimumNumberOfMovies = 0
+
+    StreamingExample.main(Array(
+      INPUT_ACTORS_TEST,
+      INPUT_RATING_EVENTS_TEST,
+      minimumNumberOfVotes.toString,
+      minimumNumberOfMovies.toString
+    ))
+
+    // ============= expected output after whole stream processed:  ==================
+    //
+    //  ==== TASK 1: busy actors ====
+    //  actor: Mr John, number of movies:  5
+    //  actor: Ms Anne, number of movies:  2
+    //  actor: Mr Rick, number of movies:  1
+    //
+    //  ==== TASK 2: best actors (at least 0 votes) ====
+    //  actor: Mr Rick, average rate:  6.0, number of votes: 1000
+    //  actor: Ms Anne, average rate:  5.4, number of votes: 5000
+    //  actor: Mr John, average rate:  4.7777777, number of votes: 9000
+    //
+    //  ==== TASK 3: best busy actors (at least 0 movies) ====
+    //  actor: Mr Rick, average rate:  6.0, number of movies: 1
+    //  actor: Ms Anne, average rate:  5.4, number of movies: 2
+    //  actor: Mr John, average rate:  4.7777777, number of movies: 5
+
+
+  }
+
+
+  @Test
+  def shouldCalculateTopActorsOnSmallTestDataSetWithFiltering() = {
+    val minimumNumberOfVotes = 6000
+    val minimumNumberOfMovies = 2
+
+    StreamingExample.main(Array(
+      INPUT_ACTORS_TEST,
+      INPUT_RATING_EVENTS_TEST,
+      minimumNumberOfVotes.toString,
+      minimumNumberOfMovies.toString
+    ))
+
+    // ============= expected output after whole stream processed:  ==================
+    //
+    //  ==== TASK 1: busy actors ====
+    //  actor: Mr John, number of movies:  5
+    //  actor: Ms Anne, number of movies:  2
+    //  actor: Mr Rick, number of movies:  1
+    //
+    //  ==== TASK 2: best actors (at least 6000 votes) ====
+    //  actor: Mr John, average rate:  4.7777777, number of votes: 9000
+    //
+    //  ==== TASK 3: best busy actors (at least 2 movies) ====
+    //  actor: Ms Anne, average rate:  5.4, number of movies: 2
+    //  actor: Mr John, average rate:  4.7777777, number of movies: 5
+
+
+  }
+
+
+}
